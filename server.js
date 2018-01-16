6b77456e3f071d2e9598e93ce5d0a095eaae03f4
Yuriy Stetskyi <yst@interlogic.com.ua>
2018-01-16 15:46:53 +0200

    Specified credentials for using Google Speech API

--- server.js
+++ server.js
@@ -3,39 +3,78 @@ const server = express();
 const http = require('http').Server(server);
 const io = require('socket.io')(http);
 const speech = require('@google-cloud/speech');
+const fs = require('fs');
+const google = require('googleapis');
 
-// Your Google Cloud Platform project ID
-// const projectId = 'your-project-id';
+const projectId = 'static-retina-192310';
+
+// Creates a client
 // const client = new speech.SpeechClient({
-//     projectId: projectId,
+//   projectId: projectId
 // });
-// const config = {
-//     encoding: 'LINEAR16',
-//     sampleRateHertz: 16000,
-//     languageCode: 'en-US',
-// };
+
+const client = new speech.SpeechClient();
+
+// The name of the audio file to transcribe
+const fileName = './audio/test1.mp3';
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+console.log(audioBytes);
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+  content: audioBytes,
+};
+const config = {
+  encoding: 'LINEAR16',
+  sampleRateHertz: 16000,
+  languageCode: 'uk-UA',
+};
+const request = {
+  audio: audio,
+  config: config,
+};
+
+// Detects speech in the audio file
+client
+  .recognize(request)
+  .then(data => {
+    const response = data[0];
+    console.log("Data: " + JSON.stringify(data));
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR OCCURED:', err);
+  });
+
 
 server.set('view engine', 'ejs');
-server.get('/', function(req, res) {
+server.get('/', function (req, res) {
     res.render('pages/googleSpeech');
 });
-server.get('/test', function(req, res) {
+server.get('/test', function (req, res) {
     res.render('pages/index');
 });
 server.use(express.static('audio'));
 
 var connectCounter = 0;
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
-    connectCounter++; 
+    connectCounter++;
     io.emit('connections', connectCounter);
 
-    socket.on('disconnect', function() { 
+    socket.on('disconnect', function () {
         connectCounter--;
         io.emit('connections', connectCounter);
     });
 
-    socket.on('message', function(buffer){
+    socket.on('message', function (buffer) {
         var base64 = new Buffer(buffer).toString('base64')
         //console.log('sample: ' + base64);
         //var request = { audio: { content: base64 }, config: config };
@@ -51,11 +90,11 @@ io.on('connection', function(socket){
         //     });
         io.emit('message', buffer);
     });
-    socket.on('stop', function(){
+    socket.on('stop', function () {
         console.log('socket:stop ' + Date.now());
     });
-  });
+});
 
-http.listen(8080, function(){
+http.listen(8080, function () {
     console.log('8080 is the magic port');
 });
