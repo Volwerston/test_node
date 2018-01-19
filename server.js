844a6b2b13b03e9ec843bb292dc4549da68569f6
Yuriy Stetskyi <yst@interlogic.com.ua>
2018-01-19 15:24:43 +0200

    Automatically assigning colors to users'

--- server.js
+++ server.js
@@ -70,39 +70,74 @@ server.use(express.static('audio'));
 /* WEBSOCKET CONFIG */
 var connectCounter = 0;
 var bufferPool = [];
+var userColors = {};
+
+function assignColor(newGuid) {
+
+  while (true) {
+    var r = Math.floor(Math.random() * Math.floor(255));
+    var g = Math.floor(Math.random() * Math.floor(255));
+    var b = Math.floor(Math.random() * Math.floor(255));
+
+    var isFound = false;
+
+    for (var key in userColors) {
+      let colorArr = userColors[key];
+      if (colorArr[0] === r && colorArr[1] === g && colorArr[2] === b) {
+        isFound = true;
+        break;
+      }
+    }
+
+    if (!isFound) {
+      userColors[newGuid] = [r, g, b];
+      break;
+    }
+  }
+
+  console.log(userColors);
+  io.emit('colors', userColors);
+}
 
 io.on('connection', function (socket) {
 
   connectCounter++;
   io.emit('connections', connectCounter);
 
-  io.emit('guid', guid.create().value);
+  var newGuid  = guid.create().value;
+  io.emit('guid', newGuid);
+  assignColor(newGuid);
 
   socket.on('disconnect', function () {
     connectCounter--;
     io.emit('connections', connectCounter);
   });
 
-  socket.on('message', function(msg){
+  socket.on('message', function (msg) {
     io.emit('message', msg);
   });
 
   socket.on('voice', function (msg) {
-    if(bufferPool[msg.guid] === undefined){
-        bufferPool[msg.guid] = new Buffer(msg.data);
+    if (bufferPool[msg.guid] === undefined) {
+      bufferPool[msg.guid] = new Buffer(msg.data);
     }
-    else{
-        bufferPool[msg.guid] = Buffer.concat([bufferPool[msg.guid], new Buffer(msg.data)]);
+    else {
+      bufferPool[msg.guid] = Buffer.concat([bufferPool[msg.guid], new Buffer(msg.data)]);
     }
     //dataBuffer = (dataBuffer === null) ? new Buffer(buffer) : Buffer.concat([dataBuffer, new Buffer(buffer)]);
+  });
+
+  socket.on('leave', function(guid){
+      delete userColors[guid];
+      io.emit('colors', userColors);
   });
 
   socket.on('endMsg', function (guid) {
 
     let base64Buffer = (bufferPool[guid] === undefined ? "" : bufferPool[guid]).toString('base64');
 
-    if(base64Buffer.length === 0) return;
-    
+    if (base64Buffer.length === 0) return;
+
     var request = { audio: { content: base64Buffer }, config: config };
 
     client
