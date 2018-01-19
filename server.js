2fb9fbec7ad9b273d3cfe406ba8d269c495d800c
Yuriy Stetskyi <yst@interlogic.com.ua>
2018-01-19 10:27:21 +0200

    Moved Start and Stop buttons to main page; removed redundant console logging

--- server.js
+++ server.js
@@ -22,39 +22,39 @@ const projectId = 'static-retina-192310';
 
 /* TEST FOR GOOGLE SPEECH API */
 // The name of the audio file to transcribe
-const fileName = './audio/my_raw5.raw';
+// const fileName = './audio/my_raw5.raw';
 
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
+// // Reads a local audio file and converts it to base64
+// const file = fs.readFileSync(fileName);
 
-const audioBytes = file.toString('base64');
+// const audioBytes = file.toString('base64');
 
-const audio = {
-  content: audioBytes,
-};
+// const audio = {
+//   content: audioBytes,
+// };
 
-const request = {
-  audio: audio,
-  config: config,
-};
+// const request = {
+//   audio: audio,
+//   config: config,
+// };
 
-console.log(audioBytes);
+// console.log(audioBytes);
 
-//Detects speech in the audio file
-client
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    console.log("Data: " + JSON.stringify(data));
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-  })
-  .catch(err => {
-    console.error('ERROR OCCURED:', err);
-  });
+// //Detects speech in the audio file
+// client
+//   .recognize(request)
+//   .then(data => {
+//     const response = data[0];
+//     console.log("Data: " + JSON.stringify(data));
+//     const transcription = response.results
+//       .map(result => result.alternatives[0].transcript)
+//       .join('\n');
+//     console.log(`Transcription: ${transcription}`);
+//   })
+//   .catch(err => {
+//     console.error('ERROR OCCURED:', err);
+//   });
 
 /* WEB SERVER CONFIG */
 server.set('view engine', 'ejs');
@@ -85,38 +85,12 @@ io.on('connection', function (socket) {
   });
 
   socket.on('voice', function (buffer) {
-
-    //let newChunk = new Buffer(buffer).toString('base64');
-    //base64Buffer += newChunk;
-
-    // console.log(newChunk);
-    // console.log("------");
-
-    // var request = { audio: { content:  base64Buffer }, config: config };
-
-    // client
-    //   .recognize(request)
-    //   .then(data => {
-    //     const response = data[0];
-    //     console.log("Data: " + JSON.stringify(data));
-    //     const transcription = response.results
-    //       .map(result => result.alternatives[0].transcript)
-    //       .join('\n');
-
-    //     console.log("Message: " + transcription);
-    //     io.emit('message', transcription);
-    //   })
-    //   .catch(err => {
-    //     console.error('ERROR:', err);
-    //   });
-
     dataBuffer = (dataBuffer === null) ? new Buffer(buffer) : Buffer.concat([dataBuffer, new Buffer(buffer)]);
   });
 
   socket.on('stop', function () {
 
     let base64Buffer = dataBuffer.toString('base64');
-    console.log(base64Buffer);
 
     var request = { audio: { content: base64Buffer }, config: config };
 
@@ -124,12 +98,10 @@ io.on('connection', function (socket) {
       .recognize(request)
       .then(data => {
         const response = data[0];
-        console.log("Data: " + JSON.stringify(data));
         const transcription = response.results
           .map(result => result.alternatives[0].transcript)
           .join('\n');
 
-        console.log("Message: " + transcription);
         io.emit('message', transcription);
         dataBuffer = null;
       })
